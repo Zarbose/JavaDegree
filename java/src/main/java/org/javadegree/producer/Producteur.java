9b5e62543d64d1556ad8f12a6bed9dcebe6a1840
zarbose <simon.pieto@gmail.com>
2023-06-02 16:39:16 +0200

    Refactoring producer constructor

--- java/src/main/java/org/javadegree/producer/Producteur.java
+++ java/src/main/java/org/javadegree/producer/Producteur.java
@@ -1,11 +1,8 @@
 package org.javadegree.producer;
 
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Producteur implements Runnable {
@@ -14,28 +11,28 @@ public class Producteur implements Runnable {
     private final int max;
     private final KafkaProducer<String, String> producer;
 
-    public Producteur(int min, int max){
+    // 172.21.0.2
+    // kafka-console-consumer.sh --topic Temperature-Celsius --bootstrap-server 172.21.0.2:9092
+
+    public Producteur(int min, int max, KafkaProducer<String, String> producer){
         this.min=min;
         this.max=max;
 
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9094");
-        // props.put("bootstrap.servers", "kafka:9092");
-        props.put("linger.ms", 1);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-        this.producer = new KafkaProducer<>(props);
+        this.producer = producer;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             int degree = (int) (this.min + (Math.random() * (this.max - this.min)));
-            producer.send(new ProducerRecord<String, String>(topic, Integer.toString(degree)));
+            producer.send(new ProducerRecord<String, String>(topic,"celsius", Integer.toString(degree)));
+
+            // System.out.println("i="+i);
+
+            producer.flush();
 
             try {
-                TimeUnit.SECONDS.sleep(3);
+                TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
