b56e6001b1871b64535b54402655f1e204ffd6c3
zarbose <simon.pieto@gmail.com>
2023-06-02 17:57:50 +0200

    docs(java): add javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.javadegree"},{"l":"org.javadegree.consumer"},{"l":"org.javadegree.producer"},{"l":"org.javadegree.producerconsumer"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.javadegree.consumer","l":"Consomeur"},{"p":"org.javadegree","l":"KafkaManager"},{"p":"org.javadegree","l":"Main"},{"p":"org.javadegree.producerconsumer","l":"ProducerConsumer"},{"p":"org.javadegree.producer","l":"Producteur"}];updateSearchResults();
