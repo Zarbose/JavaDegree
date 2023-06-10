a3510c141a9fc62ac747d2ed10eff55992ab9796
Zarbose <simonpieto.pieto@gmail.com>
2023-06-10 23:41:52 +0200

    upadte

--- java/src/main/java/org/javadegree/utils/KafkaManager.java
+++ java/src/main/java/org/javadegree/utils/KafkaManager.java
@@ -21,9 +21,8 @@ public class KafkaManager {
 
     public KafkaProducer<String, String> getProducer() {
         Properties props_pro = new Properties();
-        props_pro.put("bootstrap.servers", "localhost:9094");
-        // props.put("bootstrap.servers", "kafka:9092");
-        // props.put("linger.ms", 1);
+        // props_pro.put("bootstrap.servers", "localhost:9094");
+        props_pro.put("bootstrap.servers", "kafka:9092");
         props_pro.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props_pro.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         KafkaProducer<String, String> pro = new KafkaProducer<>(props_pro);
@@ -33,8 +32,8 @@ public class KafkaManager {
 
     public KafkaConsumer<String, String> getConsumer() {
         Properties props_cons = new Properties();
-        props_cons.put("bootstrap.servers", "localhost:9094");
-        // props.put("bootstrap.servers", "kafka:9092");
+        // props_cons.put("bootstrap.servers", "localhost:9094");
+        props_cons.put("bootstrap.servers", "kafka:9092");
         props_cons.setProperty("group.id", "test");
         props_cons.setProperty("enable.auto.commit", "true");
         props_cons.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
@@ -51,8 +50,8 @@ public class KafkaManager {
 
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
-        //props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        // props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
